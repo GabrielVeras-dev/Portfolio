52795fadcf4d015f67e4983bb6cf2054cc2b0d04
Gabriel Veras <bielveras.e@hotmail.com>
2023-10-25 19:11:56 -0300

    Corrigido Bugs e adiocionado @media
    Inserido breakpoints, configurado o media para tamanhos de 1200px, 991px e 768px.
    Corrigido bug de cor nas media.
    Corrigido função para fechar o nav quando a media estiver ativa.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
+// Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // Scroll Section
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
-
+  
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -12,17 +19,42 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            // ativar navbar links
+        if (top >= offset && top < offset + height) {
+            // active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
         }
     });
 
-// Sticky header
-let header = document.querySelector('header');
-header.classList.toggle('sticky', window.scrollY > 100)
+
+/*
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      // ativar navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  */
+
+  // Sticky header
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+// Remover o Toggle Icon quando clicar no navbar links
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 
 }
