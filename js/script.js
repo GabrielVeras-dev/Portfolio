68214816951cf5fe8fc9a8dc85b7e55b1fe59f84
Gabriel Veras <bielveras.e@hotmail.com>
2023-10-27 17:36:09 -0300

    Correção de bugs e mais adição de responsabilidade
    Corrigido alguns bugs e erros no código, e adicionais mais @media para maior responsividade no portfolio.

--- js/script.js
+++ js/script.js
@@ -28,27 +28,6 @@ window.onscroll = () => {
         }
     });
 
-
-/*
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      // ativar navbar links
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-  */
-
   // Sticky header
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
