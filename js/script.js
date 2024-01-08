51de68a22f140a52c2b2958681d1eafc43fe293a
Gabriel Veras <bielveras.e@hotmail.com>
2024-01-08 12:00:27 -0300

    Realizado alterações de animação da sessão home
    Adicionado mais estilos das animações no geral na sessão home.

--- js/script.js
+++ js/script.js
@@ -25,7 +25,13 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        } // Se quiser usar a animação que se repete na rolagem, use isto:
+        else {
+          //  sec.classList.remove('show-animate');
         }
+
     });
 
   // Sticky header
